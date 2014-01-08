0c03ce1fc1ca5b6955c30cfe553670d5a5aec29b
Ram Iyer <ramky.iyer@yahoo.com>
2014-01-08 15:45:17 -0800

    resolving bugs per john morgan

--- blackjack/solution.rb
+++ blackjack/solution.rb
@@ -30,6 +30,12 @@ def cards_to_string(cards)
   "[#{str_array.join(', ')}]"
 end
 
+def show_cards(player_cards, dealer_cards, player_total, dealer_total, player_name)
+  puts
+  puts "#{player_name} has #{cards_to_string(player_cards)}, for a total of #{player_total}"
+  puts "Dealer has #{cards_to_string(dealer_cards)}, for a total of #{dealer_total}"  
+end
+
 SUITS = {
   'H' => 'Hearts',
   'D' => 'Diamonds',
@@ -46,40 +52,42 @@ CARDS = {
 suits = %w[H D S C]
 cards = %w[2 3 4 5 6 7 8 9 10 J Q K A]
 
+print "What is the player's name? "
+player_name = gets.chomp.downcase.capitalize
+puts "Hello #{player_name}, Welcome to Blackjack!"
+
 prompt_another_game = false
+answer = 'y'
 
-while true
+while true && answer == 'y'
+  puts
   prompt_another_game = false
-  print "What is the player's name? "
-  player_name = gets.chomp.downcase.capitalize
-  puts "Hello #{player_name}, Welcome to Blackjack!"
-
   deck = suits.product cards
   deck.shuffle!
 
-  my_cards = []
+  player_cards = []
   dealer_cards = []
 
-  my_cards << deck.pop
+  player_cards << deck.pop
   dealer_cards << deck.pop
-  my_cards << deck.pop
+  player_cards << deck.pop
   dealer_cards << deck.pop
 
   dealer_total = calculate_total(dealer_cards)
-  my_total     = calculate_total(my_cards)
+  player_total     = calculate_total(player_cards)
 
   # Show Cards
 
-  puts "Dealer has #{cards_to_string(dealer_cards)}, for a total of #{dealer_total}"
-  puts "#{player_name} has #{cards_to_string(my_cards)}, for a total of #{my_total}"
+  puts "Dealer showing #{dealer_cards[0]}"
+  puts "#{player_name} has #{cards_to_string(player_cards)}, for a total of #{player_total}"
   puts 
 
-  if my_total == 21
+  if player_total == 21
     puts "#{player_name} hit blackjack! #{player_name} wins!"
     prompt_another_game = true
   end
 
-  while my_total < 21 && !prompt_another_game
+  while player_total < 21 && !prompt_another_game
     print "What would you like to do? Press h to hit or s to stay. "
     input = gets.chomp.downcase
     unless %w[h s].include?(input)
@@ -94,18 +102,20 @@ while true
 
     # Hit
     new_card = deck.pop
-    my_cards << new_card
-    my_total = calculate_total(my_cards)
+    player_cards << new_card
+    player_total = calculate_total(player_cards)
 
     puts "Dealing new card to #{player_name}."
-    puts "#{player_name} now has #{cards_to_string(my_cards)}, for a total of #{my_total}"
-    puts "Dealer has #{cards_to_string(dealer_cards)}, for a total of #{dealer_total}"
+    puts "#{player_name} now has #{cards_to_string(player_cards)}, for a total of #{player_total}"
+    puts "Dealer showing #{dealer_cards[0]}"
     puts 
 
-    if my_total == 21
+    if player_total == 21
+      show_cards(player_cards, dealer_cards, player_total, dealer_total, player_name)
       puts "#{player_name} hit blackjack! #{player_name} wins!"
       prompt_another_game = true
-    else
+    elsif player_total > 21
+      show_cards(player_cards, dealer_cards, player_total, dealer_total, player_name)
       puts "#{player_name} busted!"
       prompt_another_game = true
     end
@@ -113,6 +123,7 @@ while true
 
   # Dealer's turn
   if dealer_total == 21 && !prompt_another_game
+    show_cards(player_cards, dealer_cards, player_total, dealer_total, player_name)
     puts "Dealer hit blackjack! #{player_name} loses!"
     prompt_another_game = true
   end
@@ -123,15 +134,15 @@ while true
     dealer_cards << new_card
     dealer_total = calculate_total(dealer_cards)
 
-    puts "#{player_name} has #{cards_to_string(my_cards)}, for a total of #{my_total}"
     puts "Dealing new card to dealer."
-    puts "Dealer now has #{cards_to_string(dealer_cards)}, for a total of #{dealer_total}"
     puts
 
     if dealer_total == 21
+      show_cards(player_cards, dealer_cards, player_total, dealer_total, player_name)  
       puts "Dealer hit blackjack!  #{player_name} loses!"
       prompt_another_game = true
     elsif dealer_total > 21
+      show_cards(player_cards, dealer_cards, player_total, dealer_total, player_name)
       puts "Dealer busted! #{player_name} wins!"
       prompt_another_game = true
     end
@@ -139,9 +150,11 @@ while true
 
   # Compare hands
   unless prompt_another_game
-    if dealer_total > my_total
+    show_cards(player_cards, dealer_cards, player_total, dealer_total, player_name)
+
+    if dealer_total > player_total
       puts "#{player_name} loses."
-    elsif my_total > dealer_total
+    elsif player_total > dealer_total
       puts "#{player_name} wins."
     else
       puts "Would you believe it? It's a tie!"
@@ -149,13 +162,14 @@ while true
   end
 
   # If prompt_another_game is set correctly, the control should reach here
-  print "Do you want to play another game[y/n]? "
-  answer = gets.chomp.downcase
-  unless %w[y n].include?(answer)
-    puts "Error: You must enter y or n."
-    next    
-  end
-  if answer == 'n'
-    break
+  while true
+    print "Do you want to play another game[y/n]? "
+    answer = gets.chomp.downcase
+    unless %w[y n].include?(answer)
+      puts "Error: You must enter y or n."
+      next    
+    else
+      break
+    end
   end
 end
